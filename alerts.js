8b9047c7b1e15a1ee1081573401f2ae9f77fe212
p3lim <adrianlund@gmail.com>
2014-01-24 23:02:18 +0100

    Query for server states on server start

--- alerts.js
+++ alerts.js
@@ -9,8 +9,38 @@ var worlds = {
 	25: {name: 'Briggs'}
 }
 
+var http = require('http');
+var query = function(params, callback){
+	http.get('http://census.soe.com/get/ps2:v2/' + params, function(response){
+		var result = '';
+		response.on('data', function(chunk){
+			result += chunk;
+		});
+
+		response.on('end', function(){
+			callback(JSON.parse(result));
+		});
+	});
+}
+
 module.exports = function(){
 	var self = this;
+	self.update = function(){
+		query('world?c:limit=100', function(result){
+			for(var index = 0; index < result.world_list.length; index++){
+				var data = result.world_list[index];
+				var world = worlds[+data.world_id];
+				if(world){
+					if(data.state == 'online')
+						self.updateWorld(world);
+				}
+			}
+		});
+	}
+
+	self.updateWorld = function(world){
+
+	}
 
 	for(var index in worlds){
 		var world = worlds[index];
@@ -20,6 +50,8 @@ module.exports = function(){
 		world.eventID = 0;
 		world.alert = {};
 	}
+
+	self.update();
 
 	return self;
 }
