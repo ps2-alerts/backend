d41a00aed76434c7fcc2a54e6046ffdd298deb56
p3lim <adrianlund@gmail.com>
2014-01-24 22:30:38 +0100

    Add default port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 var server = require('http').createServer(app);
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 5000);
 
 var wss = require('ws').Server;
 var sockets = new wss({server: server});
