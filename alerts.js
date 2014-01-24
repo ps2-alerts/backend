529554fca34ce5d4c946fec653d7c866f474cac5
p3lim <adrianlund@gmail.com>
2014-01-24 22:57:25 +0100

    Set default world details

--- alerts.js
+++ alerts.js
@@ -1,4 +1,25 @@
+var worlds = {
+	1: {name: 'Connery'},
+	9: {name: 'Woodman'},
+	10: {name: 'Miller'},
+	11: {name: 'Ceres'},
+	13: {name: 'Cobalt'},
+	17: {name: 'Mattherson'},
+	18: {name: 'Waterson'},
+	25: {name: 'Briggs'}
+}
+
 module.exports = function(){
 	var self = this;
+
+	for(var index in worlds){
+		var world = worlds[index];
+		world.id = index;
+		world.state = '';
+		world.active = false;
+		world.eventID = 0;
+		world.alert = {};
+	}
+
 	return self;
 }
